0edddcdab2176d504a889f196dae30a13953372d
Andradev101 <>
2021-08-13 08:43:18 +0000

    huh?

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 var cors = require('cors');
 require('dotenv').config()
 
@@ -11,8 +13,18 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"), uploadFiles);
 
-
+function uploadFiles(req, res) {
+    console.log(req.file);
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+      });
+}
+//that was easy
+//still stuck on the exercise tracker though.
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
